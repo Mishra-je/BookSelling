7850c0603343a08d4a1173cdf90606159fb59dcd
Airbnb-Project <skumarmishra579@gmail.com>
2025-03-27 19:59:22 +0530

    production ready

--- Backend/index.js
+++ Backend/index.js
@@ -9,15 +9,23 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 const AuthRoutes = require('./Routes/AuthRoutes')
 const BookRoutes = require('./Routes/BookRoutes')   
+const path = require('path')
 // const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(morgan('dev'))
+const _dirname = path.resolve();
+
 
 app.use('/auth',AuthRoutes)
 app.use('/book', BookRoutes)
+app.use(express.static(path.join(_dirname, "/Front-end/dist")))
+app.get('*',(req,res) => {
+    res.sendFile(path.resolve(_dirname,"Front-end", "dist","index.html" ))
+})
+
 
 app.get("/",(req,res) => {
     res.send("Welcome to My Hotel")
